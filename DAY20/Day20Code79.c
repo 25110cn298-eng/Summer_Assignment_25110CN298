fd00d46d613368070db441002a8ab4d47d198a62
25110cn298-eng <25110cn298@glbitm.ac.in>
2026-06-22 00:02:38 +0530

    Add files via upload

---
+++ DAY20/Day20Code79.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+int main(){
+    int a[10][10], r, c, sum;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Row-wise Sum:\n");
+    for(int i = 0; i < r; i++){
+        sum = 0;
+        for(int j = 0; j < c; j++){
+            sum += a[i][j];
+        }
+        printf("Sum of Row %d = %d\n", i + 1, sum);
+    }
+    return 0;
+}
